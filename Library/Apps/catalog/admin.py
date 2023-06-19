3462bb1c5ff1ecb69d1fcf157d145e8ca16f64da
yeyderjhjl <jturpoan.@unsa.edu.pe>
2023-06-19 14:32:51 -0500

    Configuración vistas de lista

--- Library/Apps/catalog/admin.py
+++ Library/Apps/catalog/admin.py
@@ -1,3 +1,23 @@
 from django.contrib import admin
+from .models import Book, Author, Genre, BookInstance, Language
 
 # Register your models here.
+
+#admin.site.register(Book)
+@admin.register(Book) # = adimn.site.register(Book, BokkAdmin)
+class BookAdmin(admin.ModelAdmin):
+    list_display=('title', 'author', 'display_genre')
+
+#admin.site.register(Author)
+class AuthorAdmin(admin.ModelAdmin):
+    list_display=('last_name', 'first_name', 'date_of_birth', 'date_of_death')
+admin.site.register(Author, AuthorAdmin)
+
+#admin.site.register(BookInstance)
+@admin.register(BookInstance)
+class BookInstanceAdmin(admin.ModelAdmin):
+    list_filter=('status', 'due_back')
+
+admin.site.register(Genre)
+#adicional 
+admin.site.register(Language)
