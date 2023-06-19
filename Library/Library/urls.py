d50e6cd8c199b63945df8b28bc460fdc4d4ae51c
yeyderjhjl <jturpoan.@unsa.edu.pe>
2023-06-18 23:51:33 -0500

    Creación del proyecto Library

--- Library/Library/urls.py
+++ Library/Library/urls.py
@@ -1,5 +1,5 @@
 """
-URL configuration for myweb project.
+URL configuration for Library project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/4.2/topics/http/urls/
