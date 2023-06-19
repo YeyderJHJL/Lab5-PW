d15e1a28d131dc5ce8449b931a8b9c3660b545b5
yeyderjhjl <jturpoan.@unsa.edu.pe>
2023-06-19 15:06:24 -0500

    Ajustes yadministración de la vista de los campos

--- Library/Apps/catalog/admin.py
+++ Library/Apps/catalog/admin.py
@@ -3,20 +3,44 @@ from .models import Book, Author, Genre, BookInstance, Language
 
 # Register your models here.
 
+#para introducir en BookAdmin
+class BookInstanceInline(admin.TabularInline): # tambien puede ser StackedInline
+    model = BookInstance
+
 #admin.site.register(Book)
-@admin.register(Book) # = adimn.site.register(Book, BokkAdmin)
+@admin.register(Book) # = adimn.site.register(Book, BookAdmin)
+
 class BookAdmin(admin.ModelAdmin):
     list_display=('title', 'author', 'display_genre')
+    inlines=[BookInstanceInline]
+
+#---------------------
+class BookInlines(admin.TabularInline):
+    model = Book
 
 #admin.site.register(Author)
+@admin.register(Author)
+
 class AuthorAdmin(admin.ModelAdmin):
     list_display=('last_name', 'first_name', 'date_of_birth', 'date_of_death')
-admin.site.register(Author, AuthorAdmin)
+    fields=('last_name', 'first_name', ('date_of_birth', 'date_of_death'))
+    inlines=[BookInlines]
+
 
 #admin.site.register(BookInstance)
 @admin.register(BookInstance)
+
 class BookInstanceAdmin(admin.ModelAdmin):
+    list_display=('status', 'due_back', 'id')
     list_filter=('status', 'due_back')
+    fieldsets=(
+        (None, {
+            'fields': ('book', 'imprint', 'id')
+        }),
+        ('Availability', {
+            'fields': ('status', 'due_back')
+        }),
+    )
 
 admin.site.register(Genre)
 #adicional 
